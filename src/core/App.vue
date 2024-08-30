fad4b4f8c5574e8c966072ab7ce2545c165260ce
J2mF <mp4-12cs5@hotmail.com>
2024-08-30 13:51:45 +0300

    feat: initial project structure

---
+++ src/core/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <component :is="route?.meta?.header ?? ''"></component>
+  <RouterView />
+  <component :is="route?.meta?.footer ?? ''"></component>
+</template>
+
+<script setup lang="ts">
+import { RouterLink, RouterView, useRouter } from 'vue-router'
+const route = useRouter()
+</script>
+
+<style scoped></style>
